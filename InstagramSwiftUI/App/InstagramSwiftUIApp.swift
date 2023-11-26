1e37ddbc1162f36ce523026c82e1af0d7847fab8
Eldar <114824844+Gaiypovv@users.noreply.github.com>
2023-11-26 11:34:30 +0600

    Initial Commit

---
+++ InstagramSwiftUI/App/InstagramSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUIApp.swift
+//  InstagramSwiftUI
+//
+//  Created by Eldar Gaiypov on 26/11/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InstagramSwiftUI/Core/Profile/ProfileView.swift
@@ -0,0 +1,37 @@
+//
+//  ProfileView.swift
+//  InstagramSwiftUI
+//
+//  Created by Eldar Gaiypov on 26/11/23.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        VStack {
+            // header
+            VStack{
+                // pich and stats
+                HStack{
+                    
+                }
+                
+                // name and bio
+                VStack{
+                    
+                }
+                // action button
+                
+                Divider()
+            }
+            
+            
+            // post grid view
+        }
+    }
+}
+
+#Preview {
+    ProfileView()
+}
